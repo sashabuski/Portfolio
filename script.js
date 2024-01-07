72fb4f2d26cf6e4916ec9e24b6e65f8aca3cfc03
Sasha <sashabuskin12@gmail.com>
2024-01-08 01:32:46 +1300

    Touch pointer bug fixed1. Social link animation.

--- script.js
+++ script.js
@@ -11,6 +11,18 @@ document.onmousemove = event => {
   }
 
 };
+
+document.ontouchmove = event => {
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
+
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+  }
+
+};
 const coords = { x: 0, y: 0 };
 const circles = document.querySelectorAll(".circle");
 
